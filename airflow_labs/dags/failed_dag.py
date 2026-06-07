ec239c21693c74428cd7db5f5e2f57f54dd5ee80
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2026-06-07 23:12:24 +0000

    added three dag files

---
+++ airflow_labs/dags/failed_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.exceptions import AirflowException
+from datetime import datetime
+import requests
+
+def hello_airflow():
+    print("Hello, Airflow! This is a DAG with a graph.")
+    print("Raising an AirflowException error to show fail task")
+    raise AirflowException("Forced Failed Task")
+
+def call_api(latitude, longitude):
+    api = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={latitude}&longitude={longitude}&hourly=birch_pollen,grass_pollen"
+    try:
+        response = requests.get(api)
+        response.raise_for_status()
+        data = response.json()
+        print(f"Air Quality Data for Latitude: {latitude}, Longitude: {longitude}")
+        print(data)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+with DAG(
+    dag_id = "first_dag_with_graph",
+    start_date = datetime(2026, 6, 6),
+    schedule_interval = '@daily',
+    catchup = False
+)as dag:
+    
+    task1 = PythonOperator(
+        task_id = 'hello_airflow',
+        python_callable = hello_airflow
+    )
+
+    task2 = PythonOperator(
+        task_id = "call_api",
+        python_callable = call_api,
+        op_kwargs = {
+            "latitude": 40.7128,
+            "longitude": -74.0060
+        }
+    )
+
+    task1 >> task2
